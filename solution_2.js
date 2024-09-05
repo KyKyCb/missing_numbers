4c274052b7c6414d1b6c84d14194c02706b5365e
Andrew <79220463+KyKyCb@users.noreply.github.com>
2024-09-05 21:00:32 +0300

    Create solution_2.js

---
+++ solution_2.js
@@ -0,0 +1,21 @@
+const sequenceRange = 11;
+const sequence = [1, 3, 4, 5, 7, 8, 9, 10, 11];
+
+//Более лаконичное решение :)
+
+//Сложность алгоритма O(2n) = O(n) - нужно лишь создать множество со всеми числами.
+//Если на вход принимать готовое множество то фактически будет O(n)
+
+//Сложность памяти O(2n) = O(n) - если на вход будет множество чисел, то будет фактически O(n)
+
+const findMissingNumbers = (sequence, sequenceRange) => {
+  const sequenceSet = new Set()
+  for(let i=1; i<=sequenceRange; i++){
+    sequenceSet.add(i)
+  }
+
+  return sequenceSet.difference(new Set(sequence))
+  
+};
+
+console.log("Missing numbers: ", findMissingNumbers(sequence, sequenceRange));
